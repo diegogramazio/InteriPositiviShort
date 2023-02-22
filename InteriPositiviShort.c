7dcf5551ff68228e7d8e2983ce33b3a2ed5350b4
diegogramazio <116880984+diegogramazio@users.noreply.github.com>
2023-02-22 12:24:01 +0100

    Create InteriPositiviShort.c

---
+++ InteriPositiviShort.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* scrivere un programma che stampa tutti gli interi
+ * positivi rappresentati nel tipo short */
+
+int main() { 
+	short n = 1;
+	while(n>0) {
+		    printf("%d\n", n);
+			n++;
+	}
+}		
+	
